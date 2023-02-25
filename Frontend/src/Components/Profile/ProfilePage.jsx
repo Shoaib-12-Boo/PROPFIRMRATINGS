8f96e2f2eabe2876868462b3fb98890f3bc46f49
AyanNaseer <ayannaseer70065988@gmail.com>
2023-02-25 18:31:46 +0500

    Make ProfilePage

--- Frontend/src/Components/Profile/ProfilePage.jsx
+++ Frontend/src/Components/Profile/ProfilePage.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import {useNavigate} from 'react-router-dom'
 import style from "./Profile.module.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const ProfilePage = () => {
+  const navigate = useNavigate()
+  let arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
   const user = useSelector((store) => store.userSection);
-
+  console.log(user)
+  const settings = {
+    infinite: true,
+    speed: 500,
+    autoplay:true,
+    centerMode:true,
+    slidesToShow: 1,
+    useCSS:true,
+    // fade:true,
+    slidesToScroll: 1
+  };
   return (
     <div>
       <div className={`${style.heroSec}`}></div>
@@ -16,7 +32,7 @@ const ProfilePage = () => {
         <h3 className={`mt-3 fw-semibold`}>{user.user_name}</h3>
         <h5>{user.user_email}</h5>
       </div>
-      <div>
+      {/* <div>
         <div>
           <div
             id="carouselExampleControls"
@@ -423,6 +439,56 @@ const ProfilePage = () => {
             </button>
           </div>
         </div>
+      </div> */}
+      <div>
+        <div className={`container text-center`}>  
+      <Slider {...settings}>
+        {arr.map((item)=>{
+          return  <div
+                    className="col-md-5 text-start col-lg-3 col-sm-6 shadow rounded-3 mb-2 px-3 py-3"
+                  >
+                    <div>
+                      <div>
+                        <span>
+                          <i
+                            className={`fa-solid fs-6 border ms-1 p-1 ${style.starbgGood} fa-sharp fa-star`}
+                          />
+                          <i
+                            className={`fa-solid fs-6 border ms-1 p-1 ${style.starbgGood} fa-sharp fa-star`}
+                          />
+                          <i
+                            className={`fa-solid fs-6 border ms-1 p-1 ${style.starbgGood} fa-sharp fa-star`}
+                          />
+                          <i
+                            className={`fa-solid fs-6 border ms-1 p-1 ${style.starbgGood} fa-sharp fa-star`}
+                          />
+                          <i
+                            className={`fa-solid fs-6 border ms-1 p-1 ${style.starbgGood} fa-sharp fa-star`}
+                          />
+                        </span>
+                      </div>
+                      <div className={`my-3`}>
+                        <h5>Binance</h5>
+                        <p>
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Consequatur consectetur expedita debitis
+                          corrupti aperiam non iste similique quae delectus
+                          dicta?
+                        </p>
+                      </div>
+                      <div className={`d-flex justify-content-between`}>
+                        <div className={`d-flex`}>
+                        <div className={`rounded-circle bg-dark p-2`}>
+                          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        </div>
+                        <h6 className={`mt-2 ms-2`}>{user.user_name}</h6>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+        })}
+      </Slider>
+      </div>
       </div>
     </div>
   );
